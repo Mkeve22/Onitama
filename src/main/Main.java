9a416fcb9ff4104cf00098c8de7638105f304e16
Medvei Keve <231327344+Mkeve22@users.noreply.github.com>
2025-11-18 22:30:16 +0100

    Board

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,14 @@
+import view.BoardFrame;
 import view.LoadMenu;
 import view.MainMenu;
 import view.NewGameMenu;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new MainMenu();
+        JFrame frame = new BoardFrame();
         frame.setVisible(true);
     }
 }
